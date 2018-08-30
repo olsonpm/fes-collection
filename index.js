adae9f04dd7b0d659df5b9875eac9e3273a6e88e
Philip Olson <philip.olson@pm.me>
2018-08-30 13:49:52 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//
+// don't edit this file.  It is generated via `fes create-entries`
+//
+// check whether it's in sync with your lib directory via `fes checkup`
+//
+
+import { createUtility } from 'fes'
+
+import map_definition from './lib/map'
+const map = createUtility(map_definition)
+
+export {
+  map,
+}
